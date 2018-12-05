4869ca38cb20927ae34f3fcff7d68498eb7e32fb
vipatrascu <vipatrascu@deloittece.com>
2018-12-05 11:58:39 +0200

    Delete comments

--- src/functions/getPreparersLambda.js
+++ src/functions/getPreparersLambda.js
@@ -17,22 +17,3 @@ const getPreparers = async () => {
 }
 
 module.exports = getPreparers
-
-// const getPreparersList = require('../services/getPreparersListService')
-
-// const getPreparers = async () => {
-//   return getPreparersList(__dirname, '../../tests/resources/mock-preparers.json')
-//     .then((preparers) => {
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify(preparers)
-//       }
-//     }, (error) => {
-//       return {
-//         statusCode: error.statusCode,
-//         body: JSON.stringify(error.body)
-//       }
-//     })
-// }
-
-// module.exports = getPreparers
--- src/services/getPreparersListService.js
+++ src/services/getPreparersListService.js
@@ -13,24 +13,3 @@ const getPreparersList = (dirName, jsonPath) => {
 }
 
 module.exports = getPreparersList
-
-// const fs = require('fs')
-// const path = require('path')
-// const HttpResponseStatus = require('../models/HTTPResponseStatus')
-
-// const getPreparersList = (dirName, jsonPath) => {
-//   return new Promise((resolve, reject) => {
-//     const result = JSON.parse(fs.readFileSync(path.resolve(dirName, jsonPath), 'utf8'))
-//     if (result.length === 0) {
-//       reject(new HttpResponseStatus(404, 'Preparers not found'))
-//     } else {
-//       try {
-//         resolve(result)
-//       } catch (error) {
-//         reject(new HttpResponseStatus(500, error.stack))
-//       }
-//     }
-//   })
-// }
-
-// module.exports = getPreparersList
