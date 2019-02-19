af5fd0b2096fd2d87e2cbd5ba92fbe2f945c84c3
nblore <nblore@deloitte.co.uk>
2019-02-19 12:10:36 +0000

    Updated Build & Seed job

--- Jenkinsfile/Build.groovy
+++ Jenkinsfile/Build.groovy
@@ -1,9 +1,9 @@
 def label = "jenkins-node-${UUID.randomUUID().toString()}"
 podTemplate(label: label, containers: [
     containerTemplate(name: 'dynamodb',
-                      image: 'amazon/dynamodb-local',
-                      command: 'java -jar /home/dynamodblocal/DynamoDBLocal.jar -inMemory -sharedDb -port 8001',
-                      ports: [portMapping(name: 'dynamoport', containerPort: 8001, hostPort: 8001)]),
+                      image: '086658912680.dkr.ecr.eu-west-1.amazonaws.com/cvs/dynamodb-local:latest',
+                      command: 'java -jar /home/dynamodblocal/DynamoDBLocal.jar -inMemory -sharedDb -port 8006',
+                      ports: [portMapping(name: 'dynamoport', containerPort: 8006, hostPort: 8006)]),
     containerTemplate(name: 'node', image: '086658912680.dkr.ecr.eu-west-1.amazonaws.com/cvs/nodejs-builder:latest', ttyEnabled: true, alwaysPullImage: true, command: 'cat'),]){
     node(label) {
 
@@ -16,8 +16,7 @@ podTemplate(label: label, containers: [
             withFolderProperties{
                 LBRANCH="${env.BRANCH}".toLowerCase()
             }
-
-
+            
             stage ("npm deps") {
                 sh "npm install"
             }
@@ -27,12 +26,36 @@ podTemplate(label: label, containers: [
                  sh "git secrets --scan"
                  sh "git log -p | scanrepo"
             }
+
+            stage ("credentials") {
+               withCredentials([usernamePassword(credentialsId: 'dummy-credentials', passwordVariable: 'SECRET', usernameVariable: 'KEY')]) {
+                   sh "sls config credentials --provider aws --key ${KEY} --secret ${SECRET}"
+                }
+            } 
+            stage ("create-seed-table") {
+
+                sh '''
+                aws dynamodb create-table \
+                --table-name cvs-local-preparers \
+                --attribute-definitions \
+                    AttributeName=preparerId,AttributeType=S \
+                --key-schema AttributeName=preparerId,KeyType=HASH \
+                --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --region=eu-west-1 --endpoint-url http://localhost:8006
+                '''
+
+                sh "sls dynamodb seed --seed=preparers"
+            }
             stage ("sonar") {
                 sh "npm run sonar-scanner"
             }
 
             stage ("unit test") {
                 sh "npm run test"
+            } 
+
+            stage ("integration test") {
+                sh "BRANCH=local node_modules/gulp/bin/gulp.js start-serverless"
+                sh "BRANCH=local node_modules/.bin/mocha tests/**/*.intTest.js"
             }
 
             stage("zip dir"){
@@ -42,6 +65,7 @@ podTemplate(label: label, containers: [
                 sh "cp -r src/* ${LBRANCH}/"
                 sh "cp -r node_modules ${LBRANCH}/node_modules"
                 sh "cd ${LBRANCH} && zip -qr ../${LBRANCH}.zip *"
+                
             }
 
             stage("upload to s3") {
@@ -50,7 +74,7 @@ podTemplate(label: label, containers: [
                            credentialsId: 'jenkins-iam',
                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
 
-                   sh "aws s3 cp ${LBRANCH}.zip s3://cvs-services/preparers/${LBRANCH}.zip --metadata sha256sum=\"\$(openssl dgst -sha256 -binary ${LBRANCH}.zip | openssl enc -base64)\""
+                  sh "aws s3 cp ${LBRANCH}.zip s3://cvs-services/preparers/${LBRANCH}.zip --metadata sha256sum=\"\$(openssl dgst -sha256 -binary ${LBRANCH}.zip | openssl enc -base64)\""
                 }
             }
         }
