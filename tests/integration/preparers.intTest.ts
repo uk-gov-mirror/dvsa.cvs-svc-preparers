4a05cedd50596c8a652eef53091869ef26a491b4
khodder <khodder@gmail.com>
2019-09-10 13:56:10 +0100

    CVSB-3329 - make int test more robust

--- tests/integration/preparers.intTest.ts
+++ tests/integration/preparers.intTest.ts
@@ -7,28 +7,13 @@ const url = "http://localhost:3003/";
 const request = supertest(url);
 import PreparersService from "../../src/services/PreparersService";
 import PreparersDAO from "../../src/models/PreparersDAO";
+const preparersData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/preparers.json"), "utf8"));
 
 describe("preparers", () => {
   describe("getPreparers", () => {
     context("when database is populated", () => {
-      let preparersService: PreparersService;
-      const preparersData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/preparers.json"), "utf8"));
-      let preparersDAO = null;
-
       beforeAll((done) => {
-        preparersDAO = new PreparersDAO();
-        preparersService = new PreparersService(preparersDAO);
-        const mockBuffer = preparersData.slice();
-
-        const batches = [];
-        while (mockBuffer.length > 0) {
-          batches.push(mockBuffer.splice(0, 25));
-        }
-
-        batches.forEach((batch) => {
-          preparersService.insertPreparerList(batch);
-        });
-
+        populateDatabase();
         done();
       });
 
@@ -45,29 +30,15 @@ describe("preparers", () => {
             done();
           });
       });
-
-      afterAll((done) => {
-        const dataBuffer = preparersData;
-
-        const batches = [];
-        while (dataBuffer.length > 0) {
-          batches.push(dataBuffer.splice(0, 25));
-        }
-
-        batches.forEach((batch) => {
-          preparersService.deletePreparerList(
-            batch.map((item: any) => {
-              return item.preparerId;
-            })
-          );
-        });
-
-        done();
-      });
     });
   });
 
   context("when database is empty", () => {
+    beforeAll((done) => {
+      emptyDatabase();
+      done();
+    });
+
     it("should return error code 404", (done) => {
       request.get("preparers").expect(404, done);
     });
@@ -79,4 +50,42 @@ describe("preparers", () => {
   afterEach((done) => {
     setTimeout(done, 500);
   });
+  afterAll((done) => {
+    populateDatabase();
+    done();
+  });
 });
+
+const populateDatabase = () => {
+  const preparersDAO = new PreparersDAO();
+  const preparersService = new PreparersService(preparersDAO);
+  const mockBuffer = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/preparers.json"), "utf8"));
+
+  const batches = [];
+  while (mockBuffer.length > 0) {
+    batches.push(mockBuffer.splice(0, 25));
+  }
+
+  batches.forEach((batch) => {
+    preparersService.insertPreparerList(batch);
+  });
+};
+
+const emptyDatabase =  () => {
+  const preparersDAO = new PreparersDAO();
+  const preparersService = new PreparersService(preparersDAO);
+  const dataBuffer = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/preparers.json"), "utf8"));
+
+  const batches = [];
+  while (dataBuffer.length > 0) {
+    batches.push(dataBuffer.splice(0, 25));
+  }
+
+  batches.forEach((batch) => {
+    preparersService.deletePreparerList(
+        batch.map((item: any) => {
+          return item.preparerId;
+        })
+    );
+  });
+};
