42c2b3f64d36d3fbaa626425243f51ad56986848
khodder <khodder@gmail.com>
2019-09-24 10:11:30 +0100

    CVSB-3329 - remove extraneous code from dbOperations

--- tests/util/dbOperations.ts
+++ tests/util/dbOperations.ts
@@ -2,12 +2,7 @@ import PreparersDAO from "../../src/models/PreparersDAO";
 import preparers from "../resources/preparers.json";
 import * as _ from "lodash";
 
-export const populateTestDatabase = async () => {
-    const full: boolean = false;
-    await populateDatabase(full);
-};
-
-export const populateDatabase = async (full = true) => {
+export const populateDatabase = async () => {
     const preparersMockDB = _.cloneDeep(preparers);
     const preparersDAO = new PreparersDAO();
 
@@ -19,34 +14,9 @@ export const populateDatabase = async (full = true) => {
     for (const batch of batches) {
         await preparersDAO.createMultiple(batch);
     }
-
-    // if (full) {
-    //     await preparersDAO.createMultiple(preparersMockDB);
-    // } else {
-    //     await preparersDAO.createMultiple(preparersMockDB.splice(0, 5));
-    // }
 };
 
-//   const batches = [];
-//   while (dataBuffer.length > 0) {
-//     batches.push(dataBuffer.splice(0, 25));
-//   }
-//
-//   batches.forEach((batch) => {
-//     preparersService.deletePreparerList(
-//         batch.map((item: any) => {
-//           return item.preparerId;
-//         })
-//     );
-//   });
-
-export const emptyTestDatabase = async () => {
-
-    const full: boolean = false;
-    await emptyDatabase(full);
-};
-
-export const emptyDatabase = async (full = true) => {
+export const emptyDatabase = async () => {
     const ids = _.cloneDeep(preparers).map((prep) => prep.preparerId);
     const preparersDAO = new PreparersDAO();
 
@@ -58,12 +28,6 @@ export const emptyDatabase = async (full = true) => {
     for (const batch of batches) {
         await preparersDAO.deleteMultiple(batch);
     }
-
-    // if (full) {
-    //     await preparersDAO.deleteMultiple(_.cloneDeep(ids));
-    // } else {
-    //     await preparersDAO.deleteMultiple(_.cloneDeep(ids).splice(0, 5));
-    // }
 };
 
 
